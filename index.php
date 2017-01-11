0f53426d70c1a1e091cd7521694ab8d3f8e79167
DESKTOP-QDU9QEO\fredy <fredyang001@gmail.com>
2017-01-10 23:27:44 -0800

    created index page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>comp4711 lab01</title>
+	</head>
+	<body>
+		<?php
+		echo "Hello world......comp4711 lab01";
+		?>
+	</body>
+</html>
