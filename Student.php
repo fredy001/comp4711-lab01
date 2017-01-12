81cc59987ec3cddce8bbf1d18080bb8b68e9705a
DESKTOP-QDU9QEO\fredy <fredyang001@gmail.com>
2017-01-11 17:48:02 -0800

    updated index page and student class

--- Student.php
+++ Student.php
@@ -3,21 +3,43 @@
  * Student class
  *
  * @author: Fred Yang
+ * @version: 1.0
  */
- class Student {
-	 function __construct() {
-		 $this->surname = '';
-		 $this->first_name = '';
-		 $this->emails = array();
-		 $this->grades = array();
-	 }
-	 
-	 function add_email($which, $address) {
-		 $this->emails[$which] = $address;
-	 }
-	 
-	 function add_grade($grade) {
-		 $this->grades[] = $grade;
-	 }
+class Student {
+	// construct
+	function __construct() {
+		$this->surname = '';
+		$this->first_name = '';
+		$this->emails = array();
+		$this->grades = array();
+	}
+	
+	// mutator method: add_email
+	function add_email($which, $address) {
+		$this->emails[$which] = $address;
+	}
+	
+	// mutator method: add_grade
+	function add_grade($grade) {
+		$this->grades[] = $grade;
+	}
+	
+	// calculate the average of grades
+	function average() {
+		$total = 0;
+		foreach ($this->grades as $value)
+			$total += $value;
+		return $total / count($this->grades);
+	}
+	
+	// build a fancy text representation for reporting
+	function toString() {
+		$result = $this->first_name . ' ' . $this->surname;
+		$result .= ' ('.$this->average(). ")\n";
+		foreach ($this->emails as $which=>$what)
+			$result .= $which . ': ' . $what . "\n";
+		$result .= "\n";
+		return '<pre>' . $result . '</pre>';
+	}
  }
 ?>
--- index.php
+++ index.php
@@ -6,7 +6,51 @@
 	</head>
 	<body>
 		<?php
-		echo "Hello world......comp4711 lab01.";
+		include('Student.php');
+		
+		// student array
+		$students = array();
+		
+		// add first student
+		$first = new Student();
+		$first->surname = "Doe";
+		$first->first_name = "John";
+		$first->add_email('home','john@doe.com');
+		$first->add_email('work','jdoe@mcdonalds.com');
+		$first->add_grade(65);
+		$first->add_grade(75);
+		$first->add_grade(55);
+		$students['j123'] = $first;
+		
+		// add second student
+		$second = new Student();
+		$second->surname = "Einstein";
+		$second->first_name = "Albert";
+		$second->add_email('home','albert@braniacs.com');
+		$second->add_email('work1','a_einstein@bcit.ca');
+		$second->add_email('work2','albert@physics.mit.edu');
+		$second->add_grade(95);
+		$second->add_grade(80);
+		$second->add_grade(50);
+		$students['a456'] = $second;
+
+		// add third student
+		$third = new Student();
+		$third->surname = "Yang";
+		$third->first_name = "Fred";
+		$third->add_email('home','fredyang001@gmail.com');
+		$third->add_email('work','myang71@my.bcit.ca');
+		$third->add_grade(95);
+		$third->add_grade(85);
+		$third->add_grade(75);
+		$students['c789'] = $third;
+		
+		// sort the list (by first name)
+		ksort($students);
+		
+		// display the list of students
+		foreach ($students as $student)
+			echo $student->toString();
 		?>
 	</body>
 </html>
